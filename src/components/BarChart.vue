1d8cd884a927ff0b80cf5eba4c098393722625e1
draichi <lucasdraichi@gmail.com>
2020-07-12 02:23:00 -0300

    fix(chart): removes suggested min/max of bar chart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -29,8 +29,6 @@ export default {
             zeroLineColor: 'transparent',
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 120,
             padding: 20,
             fontColor: '#9e9e9e',
           },
